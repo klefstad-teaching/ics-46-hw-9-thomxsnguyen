8fdb607df0f0848aa64ff97398a6df208ca2042f
thomxsnguyen <thomasn8255@gmail.com>
2025-03-16 08:41:23 +0000

    fixed print_path

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -56,7 +56,7 @@ void print_path(const vector<int>& path, int total) {
     }
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i];
-        if (i != path.size() - 1) cout << " -> ";
+        if (i != path.size() - 1) cout << " "; 
     }
-    cout << " (Total cost: " << total << ")" << endl;
+    cout << " \nTotal cost is " << total << endl;
 }
