11c034d04f035977b96dde53cf3c01ccb7f05af0
thomxsnguyen <thomasn8255@gmail.com>
2025-03-16 10:07:58 +0000

    fixed?

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -5,34 +5,45 @@
 #include <queue>
 #include <stack>
 #include <limits>
+#include <algorithm>
 
 using namespace std;
 
+struct Node {
+    int vertex;
+    int weight;
+    Node(int v, int w) : vertex(v), weight(w) {}
+    
+    bool operator>(const Node& other) const {
+        return weight > other.weight;
+    }
+};
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
     int n = G.numVertices;
     vector<int> distances(n, INF);
     previous.assign(n, -1);
     
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0, source});
+    priority_queue<Node, vector<Node>, greater<Node>> pq;
+    pq.push(Node(source, 0));
     distances[source] = 0;
     
     while (!pq.empty()) {
-        int dist = pq.top().first;
-        int u = pq.top().second;
+        Node current = pq.top();
         pq.pop();
+        int u = current.vertex;
+        int dist = current.weight;
         
-        if (dist > distances[u]) continue;
+        if (dist > distances[u])
+            continue;
         
         for (const Edge& edge : G[u]) {
             int v = edge.dst;
             int weight = edge.weight;
-            
             if (distances[u] + weight < distances[v]) {
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
-                pq.push({distances[v], v});
+                pq.push(Node(v, distances[v]));
             }
         }
     }
@@ -43,7 +54,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
     if (distances[destination] == INF) {
         return {};  
     }
-
+    
     vector<int> path;
     for (int at = destination; at != -1; at = previous[at]) {
         path.push_back(at);
@@ -52,7 +63,6 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
     return path;
 }
 
-
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
         cout << "No path found" << endl;
@@ -60,7 +70,8 @@ void print_path(const vector<int>& path, int total) {
     }
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i];
-        if (i != path.size() - 1) cout << " "; 
+        if (i != path.size() - 1)
+            cout << " ";
     }
-    cout << " \nTotal cost is " << total << endl; 
+    cout << " \nTotal cost is " << total << endl;
 }
