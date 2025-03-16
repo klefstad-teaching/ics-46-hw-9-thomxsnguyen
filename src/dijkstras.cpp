5ee85904639b45ed325c9ca7ee9fbce967a70909
thomxsnguyen <thomasn8255@gmail.com>
2025-03-16 10:39:26 +0000

    fixed

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -5,6 +5,7 @@
 #include <queue>
 #include <stack>
 #include <limits>
+#include <algorithm>
 
 using namespace std;
 
@@ -22,7 +23,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     int n = G.numVertices;
     vector<int> distances(n, INF);
     previous.assign(n, -1);
-    vector<bool> visited(n, false);  
+    vector<bool> visited(n, false);
 
     priority_queue<Node, vector<Node>, greater<Node>> pq;
     pq.push(Node(source, 0));
@@ -40,7 +41,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
             int v = edge.dst;
             int weight = edge.weight;
 
-            if (!visited[v] && distances[u] + weight < distances[v]) {
+            if (distances[u] + weight < distances[v]) {
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
                 pq.push(Node(v, distances[v]));
@@ -52,7 +53,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
     if (distances[destination] == INF) {
-        return {};  
+        return {};
     }
 
     vector<int> path;
@@ -68,11 +69,12 @@ void print_path(const vector<int>& path, int total) {
         cout << "No path found" << endl;
         return;
     }
+
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i];
-        if (i != path.size() - 1) 
-            cout << " ";  
+        if (i != path.size() - 1)  
+            cout << " ";
     }
-    cout << "\nTotal cost is " << total << endl; 
-}
 
+    cout << "\nTotal cost is " << total << endl;
+}
