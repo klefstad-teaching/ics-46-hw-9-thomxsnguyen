2dd332bf1e4d8b8bbbacc43fbea4cedf86f3ea08
thomxsnguyen <thomasn8255@gmail.com>
2025-03-16 10:42:39 +0000

    fixed my_assert

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -148,14 +148,15 @@ void print_word_ladder(const vector<string>& ladder) {
 
 
 void verify_word_ladder() {
+    #define my_assert(e) { cout << #e << ((e) ? " passed" : " failed") << endl; }
 
     set<string> word_list;
     load_words(word_list, "words.txt");
+
     my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
     my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
     my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
     my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
     my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
     my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
-
 }
